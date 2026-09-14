acce4f55d0109e06a1d40ad6e189e3bd7cff7dfb
mattzaitsev <mattzaitsev@gmail.com>
2026-09-14 15:15:27 -0700

    Add DMZ Pumps theme files

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<main class="dmz-content">
+  <h1>Page not found.</h1>
+  <p>The page you requested could not be found.</p>
+  <p><a class="dmz-btn red" href="<?php echo esc_url(home_url('/')); ?>">Back to Home</a></p>
+</main>
+<?php get_footer(); ?>
